330a814b05a67f116445c8a393816444bc8238cc
tanvigorre <tanvigorre@localhost>
2023-10-05 10:58:40 -0500

    We made a thingy

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -6,6 +6,17 @@ import edu.princeton.cs.introcs.StdDraw;
 
 public class Flag {
 	public static void main(String[] args) {
+		StdDraw.setXscale(0, 1);
+		StdDraw.setYscale(0, 1);
+		Color lavendar = new Color(220,220,255);
+		StdDraw.setPenColor(lavendar);
+		StdDraw.filledRectangle(0, 0, 1, 1);
+		Color orange = new Color(255,141,0);
+		StdDraw.setPenColor(orange);
+		StdDraw.filledCircle(0.5, 0.5, 0.25);
+		Color blue = new Color(0,0,240);
+		StdDraw.setPenColor(blue);
+		StdDraw.filledRectangle(0, 0, 1, 0.5);
 		
 	}
 }
